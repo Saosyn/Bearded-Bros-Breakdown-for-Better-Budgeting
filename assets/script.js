dcbc637f8708988deefc5d2d1d0b16ed5f844031
Timothy Ehli <tehli1882@gmail.com>
2024-11-12 21:56:41 -0500

    changed table

--- assets/script.js
+++ assets/script.js
@@ -51,9 +51,9 @@ function updateTable() {
 
   transactions.forEach((transaction) => {
     const row = document.createElement("tr");
-    // row.classList.add(expense.category); // Add category as class name to table row 
-    row.setAttribute('data-category', transaction.category);
-    
+    // row.classList.add(expense.category); // Add category as class name to table row
+    row.setAttribute("data-category", transaction.category);
+
     const amountText =
       transaction.amount >= 0
         ? `$${transaction.amount.toFixed(2)}`
@@ -86,7 +86,7 @@ function updateChart() {
   if (chartStatus != undefined) {
     chartStatus.destroy();
   }
-  //-- End of chart destroy   
+  //-- End of chart destroy
 
   const categoryTotals = transactions.reduce((totals, transaction) => {
     totals[transaction.category] =
@@ -131,7 +131,7 @@ function searchTable() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[2];
+    td = tr[i].getElementsByTagName("td")[3];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
